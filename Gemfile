1d05d71bf62447a10409721f0e627bb480a51b8b
Jiyoung Ko <jclareko@gmail.com>
2019-11-08 17:00:42 +0900

    added buttons

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 gem 'carrierwave', '~> 1.2'
-
+gem 'faker'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 gem 'dotenv-rails', groups: [:development, :test]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 require 'json'
 require 'open-uri'
 
+Cocktail.destroy_all
 Dose.destroy_all
 Ingredient.destroy_all
-puts "Creating seeds..."
+puts "Creating ingredients..."
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 user_serialized = open(url).read
 user = JSON.parse(user_serialized)
@@ -18,3 +20,17 @@ user['drinks'].each do |hash|
   Ingredient.create(name: hash['strIngredient1'])
 end
 
+
+
+COCKTAIL_URLS = ["https://images.pexels.com/photos/1304540/pexels-photo-1304540.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+'https://images.pexels.com/photos/1187766/pexels-photo-1187766.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+'https://images.pexels.com/photos/2093089/pexels-photo-2093089.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+'https://images.pexels.com/photos/2480828/pexels-photo-2480828.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+'https://images.pexels.com/photos/2167793/pexels-photo-2167793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500']
+
+
+COCKTAIL_URLS.each do |cocktail_url|
+  cocktail = Cocktail.new(name:Faker::ProgrammingLanguage.name)
+  cocktail.remote_photo_url = cocktail_url
+  cocktail.save
+end
